3626a9bc89c26d1d726754bc66c6ffdd3a03c4e8
Süleyman Talha Akdoğan <suleymantalhaakdogan@gmail.com>
2025-04-02 15:47:45 +0300

    Update PW1_Q1.cpp

--- PW1/PW1_Q1.cpp
+++ PW1/PW1_Q1.cpp
@@ -1,16 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int mysqrt(int number); /* This function calculates the square root of the given number. */
 
 int main(){
 
-  
-  printf("integer root: %d\n", mysqrt(409)); /* To print result to the screen */
-	printf("integer root: %d\n", mysqrt(144));
-	printf("integer root: %d", mysqrt(78));
-  return 0;
 
-
+	printf("%d\n", mysqrt(409)); /* To print result to the screen */
+	printf("%d\n", mysqrt(144));
+	printf("%d", mysqrt(78));
+	return 0;
 }
 
 
@@ -21,11 +20,9 @@ int mysqrt(int number){ /* Definition of the function. */
 	result -= 1;
 	return result;
 }
-
 /***************************
 A SAMPLE RUN IS AS FOLLOWS:
 20
 12
 8
 ****************************/
-
